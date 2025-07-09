f54e035d11f42c9038baa2a5d1dbffb166943d69
Stallone Mathenge <stallonemathenge@gmail.com>
2025-07-10 02:14:50 +0300

    UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
