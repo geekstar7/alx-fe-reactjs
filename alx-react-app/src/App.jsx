a7efd6b6c877319894240b3632887e19b91585bc
Stallone Njogu <114579512+geekstar7@users.noreply.github.com>
2025-07-10 02:10:22 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Header from './components/Header';
-import MainContent from './components/Maincontent';
+import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
 function App() {
