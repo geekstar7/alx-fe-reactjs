a7054c18a791bea438af55d8dd59834827f865b6
Stallone Njogu <114579512+geekstar7@users.noreply.github.com>
2025-07-20 23:16:07 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,9 @@
-// src/App.jsx
 import React from 'react';
 import Header from './components/Header';
-import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -12,9 +12,11 @@ function App() {
       <MainContent />
       <UserProfile name="Nairobi" age={129} bio="Capital city of Kenya, known for its wildlife and vibrant culture." />
       <UserProfile name="Tokyo" age={500} bio="A high-tech metropolis blending tradition and innovation." />
+      <Counter />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
