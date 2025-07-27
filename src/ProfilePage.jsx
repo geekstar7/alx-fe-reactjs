eb8417629a8f4fbd047b5e8cd1a8a6c88a7db2e0
Stallone Mathenge <stallonemathenge@gmail.com>
2025-07-27 20:36:51 +0300

    initial commit

---
+++ src/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
+<<<<<<< HEAD
+=======
+
+>>>>>>> eaebbdf (refactor:replace prop drilling)
