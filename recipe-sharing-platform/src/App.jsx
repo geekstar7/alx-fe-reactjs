9111441ee41c8849ca21c5a391062d3d6a4d326f
Stallone Mathenge <stallonemathenge@gmail.com>
2025-08-16 14:43:13 +0300

    updated app.jsx and main.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,23 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
 import HomePage from "./components/HomePage";
-import RecipeDetails from "./pages/RecipeDetails";
-import AddRecipe from "./pages/AddRecipe";
+import "./index.css"; // make sure Tailwind styles are included
 
 function App() {
   return (
-    <Router>
-      <nav className="bg-blue-500 text-white p-4 flex gap-4">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/recipe/1" className="hover:underline">Recipe Details</Link>
-        <Link to="/add" className="hover:underline">Add Recipe</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        <Route path="/add" element={<AddRecipe />} />
-      </Routes>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      <HomePage />
+    </div>
   );
 }
 
